75c5ac21da5e8f18f38a91e1f9d5af236dfeaaa9
Divyansh <155663331+Divodude@users.noreply.github.com>
2025-02-04 21:44:16 +0530

    Update receiver_.ino

--- receiver_/receiver_.ino
+++ receiver_/receiver_.ino
@@ -2,8 +2,8 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-#define CE_PIN D4  // D2 on ESP8266
-#define CSN_PIN D2 // D8 on ESP8266
+#define CE_PIN D4  // D4 on ESP8266
+#define CSN_PIN D2 // D2 on ESP8266
 
 RF24 radio(CE_PIN, CSN_PIN);
 const byte address[6] = "00001";  
